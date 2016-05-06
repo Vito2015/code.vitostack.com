17e3a0426e030a4c29e27a16ef7b15032610ef3f
wangye <wangye@xiaomi.com>
2016-05-06 12:02:37 +0800

    restore package requirements

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo npm install hexo --save
+sudo npm install hexo-server --save
+
+sudo npm install gulp --save
+sudo npm install gulp-minify-css --save
+sudo npm install gulp-minify-html --save
+sudo npm install gulp-uglify --save
+sudo npm install gulp-rename --save
+sudo npm install gulp-clean --save
+sudo npm install gulp-shell --save
