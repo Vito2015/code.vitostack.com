4fbc779faf5547d1f92bf40cfa9ee31dad7ddd3f
wangye <wangye@xiaomi.com>
2016-05-06 15:48:48 +0800

    repair hexo and new post

--- requirements.sh
+++ requirements.sh
@@ -2,6 +2,7 @@
 
 sudo npm install hexo --save
 sudo npm install hexo-server --save
+sudo npm install hexo-deployer-git --save
 
 sudo npm install gulp --save
 sudo npm install gulp-minify-css --save
